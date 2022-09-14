e9fee95a898b8d37918dd6b78eb47ecca1b7a6ac
rus317521 <rus317521@yandex.ru>
2022-09-14 21:43:28 +0500

    Added Main.java

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,10 +1,16 @@
 public class MagicBox<T> {
     private int count;
-    private T value;
+    protected T value;
 
-    public MagicBox(int count, T value) {
+    public <T> MagicBox(int count) {
         this.count = count;
-        this.value = value}
+        //this.value = value;
+        T arrayOfElements[] = new T[count] ;
+    }
 
     public boolean add(T item)
+    {}
+    public T pick()
+    {}
+
 }
