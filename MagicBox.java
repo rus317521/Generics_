5d3e31de8a70ebad34eaa04203b6c89683831a8e
rus317521 <rus317521@yandex.ru>
2022-09-14 20:23:28 +0500

    Added MagicBox.java

---
+++ MagicBox.java
@@ -0,0 +1,10 @@
+public class MagicBox<T> {
+    private int count;
+    private T value;
+
+    public MagicBox(int count, T value) {
+        this.count = count;
+        this.value = value}
+
+    public boolean add(T item)
+}
