cc5baa11e1fb80f456a196e7a8028112cedbb81f
rus317521 <rus317521@yandex.ru>
2022-09-18 13:40:28 +0500

    modified MagicBox, Main

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,16 +1,54 @@
+import java.util.Random;
+
 public class MagicBox<T> {
-    private int count;
-    protected T value;
+    public static int count;
+    public T value;
+    //public T[] items;
+    // protected T[] items = (T[]) new Object[count];
+    public T[] items;
 
     public <T> MagicBox(int count) {
         this.count = count;
-        //this.value = value;
-        T arrayOfElements[] = new T[count] ;
+        T[] items = (T[]) new Object[count];
+        for (int i = 0; i < items.length; i++) {
+            items[i] = null;
+        }
     }
 
-    public boolean add(T item)
-    {}
-    public T pick()
-    {}
+    public boolean add(T item) {
+        boolean b;
+        b = false;
+
+        for (int j = 0; j < count; j++) {
+            if (items[j] == null) {
+                items[j] = item;
+                b = true;
+                break;
+            }
+        }
+
+        return b;
+    }
+
+    public T pick() {
+        int randomInt = 0;
+        boolean bl = true;
+        for (int i = 0; i < MagicBox.this.items.length; i++) {
+            if (MagicBox.this.items[i] == null) {
+                bl = false;
+                int k = count - 1 - i;
+                String str = Integer.toString(k);
+                new RuntimeException("Коробка не полна, осталось заполнить " + str + " ячеек");
+                randomInt = k;
+                break;
+            }
+        }
+        if (bl == true) {
+            Random random = new Random();
+            randomInt = random.nextInt(items.length); // джава подберёт случайное число от 0 до ЧИСЛО невключительно//
+            return items[randomInt];
+        }
+        return items[randomInt];
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
 public class Main {
+    public static void main(String[] args) {
+
+        MagicBox<String> magicBox1 = new MagicBox<String>(5);
+
+        System.out.println(magicBox1.add("Stroka1"));
+        System.out.println(magicBox1.add("Stroka2"));
+        System.out.println(magicBox1.add("Stroka3"));
+        System.out.println(magicBox1.add("Stroka4"));
+        magicBox1.pick();
+        System.out.println(magicBox1.add("Stroka5"));
+        System.out.println("Случайный элемент коробки со строками: " + magicBox1.pick());
+        System.out.println(magicBox1.add("Stroka6"));
+
+        Integer[] items2 = new Integer[4];
+        MagicBox<Integer> magicBox2 = new MagicBox<>(4);
+        magicBox2.add(101);
+        magicBox2.add(102);
+        magicBox2.add(103);
+        magicBox2.pick();
+        magicBox2.add(104);
+        System.out.println("Случайный элемент коробки с числами: " + magicBox2.pick());
+        magicBox2.add(105);
+    }
 }
